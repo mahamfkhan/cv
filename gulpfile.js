bf03d0f889e772a83165b4caab8c3e057a059076
Maham Faisal Khan <34014138+mahamfkhan@users.noreply.github.com>
2024-07-13 15:46:19 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,24 @@ const scripts = require('./build/scripts');
 const images = require('./build/images');
 const sync = require('./build/browsersync');
 
-[sass, scripts, images, sync].forEach(task => {
+// Define sass task
+gulp.task('sass', function() {
+  return gulp.src('./_scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+// Apply other tasks
+[scripts, images, sync].forEach(task => {
   task(gulp);
 });
 
+// Ensure jekyll-build task is defined
+gulp.task('jekyll-build', function(done) {
+  const spawn = require('child_process').spawn;
+  return spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+// Define build task
 gulp.task('build', gulp.series(['sass', 'scripts', 'images', 'jekyll-build']));
